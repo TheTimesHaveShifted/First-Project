0663dbacedcb68180b780062d072363807e1a7ee
rmwatson5 <rmwatson5@hotmail.com>
2019-05-09 14:49:57 -0600

    added website project

---
+++ src/ZacharyWebsite/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new HomeRenderingModel();
+            model.Name = "Hello World!";
+            return this.View(model);
+        }
+    }
+}
---
+++ src/ZacharyWebsite/WebApplication1/Models/HomeRenderingModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models
+{
+    public class HomeRenderingModel
+    {
+        public string Name { get; set; }
+    }
+}
